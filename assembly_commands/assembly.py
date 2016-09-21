3742aa4bc1fd2e69a3e5e3d2b9eff40b9e63826d
linuxfreebird <linuxfreebird@gmail.com>
2016-09-21 15:55:37 -0400

    portable assembly function

--- assembly_commands/assembly.py
+++ assembly_commands/assembly.py
@@ -1,34 +1,36 @@
-sequence = '3333333333322222222122222122222222222212222222222221222222222222222112222222222222222222222212222222222222221222222222222222222122222222222212222' #set of commands to execute in sequence to spell 'hello world'
-listchr = 'abcdefghijklmnopqrstuvwxyz ' # allowed characters in a string to increment 
-sinput = '' # starting sentence to alter
-output = sinput + "" # altered sentence
-pntmax = len(output)-1 # length of sentence
-pnt = 0 # pointer to individual characters
-for chr in sequence:
-    if chr ==   '1':
-        # increment pointer
-        if pnt < pntmax:
-            pnt += 1
+def assembly(sinput,sequence,listchr):
+    output = sinput + "" # altered sentence
+    pntmax = len(output)-1 # length of sentence
+    pnt = 0 # pointer to individual characters
+    for chr in sequence:
+        if chr ==   '1':
+            # increment pointer
+            if pnt < pntmax:
+                pnt += 1
+            else:
+                pnt = 0
+        elif chr == '2':
+            # increment char
+            index = listchr.index(output[pnt])
+            if index < len(listchr)-1:
+                index += 1
+            else:
+                index = 0
+            listoutput = list(output)
+            listoutput[pnt] = listchr[index]
+            output = "".join(listoutput)
+        elif chr == '3':
+            # add char
+            output += output[0]
+            pntmax += 1
+        elif chr == '4':
+            # remove char
+            if len(output) > 0:
+                output = output[:-1]
+                pntmax -= 1
         else:
-            pnt = 0
-    elif chr == '2':
-        # increment char
-        index = listchr.index(output[pnt])
-        if index < len(listchr)-1:
-            index += 1
-        else:
-            index = 0
-        listoutput = list(output)
-        listoutput[pnt] = listchr[index]
-        output = "".join(listoutput)
-    elif chr == '3':
-        # add char
-        output += " "
-        pntmax += 1
-    else:    # '4'
-        # remove char
-        if len(output) > 0:
-            output = output[:-1]
-            pntmax -= 1
-print sinput
-print output
+            0 + 0 # do nothing
+    return output
+print assembly('432331','123443243243241224312432','1234')
+memory = [["" for y in range(8)] for x in range(8)]
+print memory
